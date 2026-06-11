ad5dcf7c0159f9d2f88fc764c67f7dc46953eb31
sliwerwolf <dewanggab@gmail.com>
2026-06-11 14:42:24 +0700

    Excel download (SheetJS), hapus file internal dari repo

--- update.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Update Dashboard TKD - Kota Yogyakarta
-cd /d "%~dp0"
-set PATH=%PATH%;C:\Program Files\Git\cmd
-echo.
-echo  Update Dashboard TKD Kota Yogyakarta
-echo  ======================================
-echo.
-python update.py
